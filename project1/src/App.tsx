c3e83509e1db92bec541aaf32facdccd7dcebd8b
saiteja-bluebenz <119647964+saiteja-bluebenz@users.noreply.github.com>
2022-12-02 12:40:56 +0530

    Learnt interfaces,Union types and differences between type interface and type annotation

--- project1/src/App.tsx
+++ project1/src/App.tsx
@@ -1,24 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
-function App() {
+// AppName is an interface that has a property called name.
+// An interface is an abstract type that the compiler uses to know which property names a given object can have.
+// Maily it is used for "type type checking"
+export interface AppName {
+  name: string;
+}
+export interface Student {
+  name: string;
+  surname: string;
+  age?: number;
+  address?: {
+    houseno: number;
+    street: string;
+    town: string;
+  };
+}
+
+function App({ name }: AppName) {
+  const [student, setStudent] = useState<Student | null>(null);
+  //In the above line of code student can be either null or student object.
+  //This is denoted by the code usestate<Studen | null>(null).
+  //This concept is Union types.
+  //Union types is nothing but when we have an object that can be of having different datatypes.
+  //  for Example 
+  //  const< student |null |boolean >(null).
+  // In this case student |null|boolean are Union Types
+  const addStudent = () => {
+    setStudent({
+      name: "Saiteja",
+      surname: "Golivi",
+      age: 24,
+    });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{name}</h1>
+      <p>
+        <b>
+          {student?.name} {student?.surname}
+        </b>
+      </p>
+      <button onClick={addStudent}>Add Studet</button>
     </div>
   );
 }
